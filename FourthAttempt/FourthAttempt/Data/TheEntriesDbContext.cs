5f9e865ba79104a3f0404762cc96fd6bc7dac6d0
Alondra Ramirez-Perez <aloram12@uw.edu>
2025-02-03 15:24:02 -0800

    Added all files of FourthAttempt project files properly

---
+++ FourthAttempt/FourthAttempt/Data/TheEntriesDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FourthAttempt.Models;
+
+namespace FourthAttempt.Data
+{
+    public class TheEntriesDbContext : DbContext
+    {
+        public TheEntriesDbContext(DbContextOptions<TheEntriesDbContext> options) : base(options) { }
+
+        public TheEntriesDbContext() { }
+
+        public DbSet<TheEntriesList> Entries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=TheSiteEntriesDB;Integrated Security=True;TrustServerCertificate=True;");
+            }
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TheEntriesList>().ToTable("entries"); 
+            // Make sure the name matches exactly
+                                                                     
+            //  modelBuilder.Entity<TheEntriesList>()
+                                                                       
+            //  .HasKey(e => e.SiteEntryID); // ✅ Explicitly set SiteEntryID as the primary key
+        }
+
+    }
+}
